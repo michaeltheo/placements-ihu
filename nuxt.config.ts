4e6dc426c3711ea4b13b7bf5a68fafe175ad5105
michaeltheo <45629601+michaeltheo@users.noreply.github.com>
2023-07-05 21:55:55 +0300

    feat: add fontaweseome item dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// @ts-ignore
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
-  css: ["@/assets/main.scss"],
+  css: ["@/assets/main.scss",
+    '@fortawesome/fontawesome-svg-core/styles.css'],
 });
