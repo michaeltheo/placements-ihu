f37607c758accf290e79d752c8b3936378aebc4b
mtheo <michaeltheochar@gmail.com>
2024-04-23 18:41:01 +0300

    fix: update admin endpoint desc

--- services/adminService.ts
+++ services/adminService.ts
@@ -20,6 +20,25 @@ interface UserResponse {
 
 
 
+/**
+ * This TypeScript function fetches users based on AM and role parameters with pagination support.
+ * @param {string} [am] - The `am` parameter in the `getUsersByAmAndRole` function stands for "Account
+ * Manager". It is used to filter users based on their account manager. If a value is provided for
+ * `am`, only users associated with that specific account manager will be retrieved. If no value is
+ * provided
+ * @param {string} [role] - The `role` parameter in the `getUsersByAmAndRole` function is used to
+ * filter users based on a specific role. If you provide a value for the `role` parameter when calling
+ * this function, only users with that specific role will be returned in the response. If you don't
+ * @param [page=1] - The `page` parameter in the `getUsersByAmAndRole` function represents the page
+ * number of results to retrieve. It defaults to 1 if not provided. This parameter is used to paginate
+ * the results, allowing you to fetch a specific page of user data from the API.
+ * @param [itemsPerPage=10] - The `itemsPerPage` parameter in the `getUsersByAmAndRole` function
+ * specifies the number of user items to be displayed per page when fetching users. By default, it is
+ * set to 10, meaning that the function will retrieve and display 10 user items per page unless a
+ * different value
+ * @returns The function `getUsersByAmAndRole` returns a Promise that resolves to a `UserResponse`
+ * object or `null`.
+ */
 export async function getUsersByAmAndRole(am?: string, role?: string, page = 1, itemsPerPage = 10): Promise<UserResponse | null> {
   try {
     const queryParams = new URLSearchParams();
