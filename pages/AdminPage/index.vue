17d8da2f0496e38c250ac2ab3210e78817271bd2
mtheo <michaeltheochar@gmail.com>
2024-05-24 01:34:00 +0300

    feat: Upadte admins page

--- pages/AdminPage/index.vue
+++ pages/AdminPage/index.vue
@@ -1,59 +1,92 @@
-<!-- eslint-disable vue/v-slot-style -->
-<!-- eslint-disable vue/valid-v-slot -->
 <template>
   <v-container class="adminPage">
-    <div class="adminPage__section--statistics">
-      <h3 class="adminPage__title">Στατιστικά Ερωτηματολογίου</h3>
-      <p class="adminPage__hint">
-        Προβολή σελίδας στατιστικών του ερωτηματολογίου των φοιτητών του
-        τμήματος.
+    <div class="adminPage__header">
+      <h2 class="adminPage__main-title">Προφίλ Διαχειριστή</h2>
+      <p class="adminPage__subtitle">
+        Επιλέξτε μία από τις παρακάτω σελίδες για να διαχειριστείτε το
+        περιεχόμενο.
       </p>
-      <BaseComponentsBaseButton
-        class="m-auto"
-        text="Σελίδα Στατιστικών"
-        @click="navigateTo('/adminPage/statistics')"
-      />
     </div>
-    <div class="adminPage__section--statistics">
-      <h3 class="adminPage__title">Πινακας Χρηστών</h3>
-      <p class="adminPage__hint">
-        Προβολή σελίδας στατιστικών του ερωτηματολογίου των φοιτητών του
-        τμήματος.
-      </p>
-      <BaseComponentsBaseButton
-        class="m-auto"
-        text="Σελίδα Χρηστών"
-        @click="navigateTo('/adminPage/users')"
-      />
-    </div>
-    <div class="adminPage__section--statistics">
-      <h3 class="adminPage__title">Πινακας Εταιρειών</h3>
-      <p class="adminPage__hint">
-        Προβολή σελίδας στατιστικών του ερωτηματολογίου των φοιτητών του
-        τμήματος.
-      </p>
-      <BaseComponentsBaseButton
-        class="m-auto"
-        text="Σελίδα Εταιρειών"
-        @click="navigateTo('/adminPage/users')"
-      />
-    </div>
-    <div class="adminPage__section--statistics">
-      <h3 class="adminPage__title">Πινακας Πρακτικών</h3>
-      <p class="adminPage__hint">
-        Προβολή σελίδας στατιστικών του ερωτηματολογίου των φοιτητών του
-        τμήματος.
-      </p>
-      <BaseComponentsBaseButton
-        class="m-auto"
-        text="Σελίδα Πρακτικης"
-        @click="navigateTo('/adminPage/users')"
-      />
-    </div>
+    <v-row class="adminPage__row" dense>
+      <v-col cols="12" sm="6" md="4">
+        <div class="adminPage__section">
+          <v-icon class="adminPage__icon" color="primary-blue-color"
+            >fa-solid fa-chart-simple</v-icon
+          >
+          <h3 class="adminPage__title">Στατιστικά Ερωτηματολογίου</h3>
+          <p class="adminPage__hint">
+            Προβολή σελίδας στατιστικών του ερωτηματολογίου των φοιτητών και των
+            εταιρειών του τμήματος.
+          </p>
+          <BaseComponentsBaseButton
+            class="adminPage__button"
+            text="Σελίδα Στατιστικών"
+            @click="navigateTo('/adminPage/statistics')"
+          />
+        </div>
+      </v-col>
+      <v-col cols="12" sm="6" md="4">
+        <div class="adminPage__section">
+          <v-icon class="adminPage__icon" color="primary-blue-color"
+            >fa-solid fa-user</v-icon
+          >
+          <h3 class="adminPage__title">Πινακας Χρηστών</h3>
+          <p class="adminPage__hint">
+            Διαχείριση χρηστώ, προβολή δικαιολογιτικών των φοιτητων. φοιτητών
+            του τμήματος.
+          </p>
+          <BaseComponentsBaseButton
+            class="adminPage__button"
+            text="Σελίδα Χρηστών"
+            @click="navigateTo('/adminPage/users')"
+          />
+        </div>
+      </v-col>
+      <v-col cols="12" sm="6" md="4">
+        <div class="adminPage__section">
+          <v-icon class="adminPage__icon" color="primary-blue-color"
+            >fa-solid fa-building</v-icon
+          >
+          <h3 class="adminPage__title">Πινακας Εταιρειών</h3>
+          <p class="adminPage__hint">
+            Διαχείριση εταιρειών και προβολή στατιστικών του ερωτηματολογίου των
+            φοιτητών του τμήματος.
+          </p>
+          <BaseComponentsBaseButton
+            class="adminPage__button"
+            text="Σελίδα Εταιρειών"
+            @click="navigateTo('/adminPage/companies')"
+          />
+        </div>
+      </v-col>
+      <v-col cols="12" sm="6" md="4">
+        <div class="adminPage__section">
+          <v-icon class="adminPage__icon" color="primary-blue-color"
+            >fa-solid fa-file-contract</v-icon
+          >
+          <h3 class="adminPage__title">Πινακας Πρακτικών</h3>
+          <p class="adminPage__hint">
+            Διαχείριση πρακτικών και προβολή στατιστικών του ερωτηματολογίου των
+            φοιτητών του τμήματος.
+          </p>
+          <BaseComponentsBaseButton
+            class="adminPage__button"
+            text="Σελίδα Πρακτικής"
+            @click="navigateTo('/adminPage/internships')"
+          />
+        </div>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const router = useRouter();
+const navigateTo = (path: string) => {
+  // Logic for navigation
+  router.push(path);
+};
+</script>
 
 <style lang="scss" scoped>
 @import "@/assets/variables.scss";
@@ -61,14 +94,49 @@
 .adminPage {
   @apply container mx-auto px-4 py-8 space-y-12;
 
-  &__section--files,
-  &__section--statistics {
-    @apply shadow-lg border border-gray-200 rounded-lg p-6 bg-white;
+  &__header {
+    @apply text-center mb-8;
+  }
+
+  &__main-title {
+    @apply text-3xl font-bold mb-2;
+    color: $primary-dark-blue-color;
+  }
+
+  &__subtitle {
+    @apply text-lg mb-4;
+    color: $primary-blue-color;
+  }
+
+  &__row {
+    @apply flex flex-wrap justify-center gap-6;
+  }
+
+  &__section {
+    @apply shadow-lg border border-gray-200 rounded-lg p-6 bg-white text-center transition-all duration-300 ease-in-out hover:shadow-xl;
+
+    &:hover {
+      transform: translateY(-7px);
+    }
+  }
+
+  &__icon {
+    @apply text-4xl mb-4;
+    color: $primary-dark-blue-color;
   }
 
   &__title {
-    @apply text-2xl md:text-3xl font-bold text-center mb-4;
+    @apply text-2xl font-bold mb-2;
     color: $primary-dark-blue-color;
+  }
+
+  &__hint {
+    @apply text-base mb-4;
+    color: $primary-blue-color;
+  }
+
+  &__button {
+    @apply m-auto  text-white py-2 px-4 rounded  transition-all;
   }
 }
 </style>
