5a626d39d9476a27c78039cc7c7c6fbd21395eb4
mtheo <michaeltheochar@gmail.com>
2024-05-26 20:01:41 +0300

    feat: addd company create update delete endpoitns

--- services/companyService.ts
+++ services/companyService.ts
@@ -1,12 +1,13 @@
 import { API_URLS } from "@/constants/apiConfig";
 import { extractErrorMessage } from "@/services/errorHandling";
-import { FetchCompanies } from "@/types/company";
+import { CompanyBase, FetchTotalCompanies, GetCompany } from "@/types/company";
+import { ErrorResponse, Message } from "@/types";
 
 export async function getAllCompanies(
   page?: number,
   itemsPerPage?: number,
   name?: string,
-): Promise<FetchCompanies> {
+): Promise<FetchTotalCompanies> {
   try {
     const params = new URLSearchParams();
 
@@ -32,12 +33,88 @@ export async function getAllCompanies(
     );
 
     if (!response.ok) {
-      return extractErrorMessage(response);
+      const errorResponse: ErrorResponse = await extractErrorMessage(response);
+      return errorResponse;
     }
 
     const data = await response.json();
     return data;
   } catch (error) {
     throw new Error("Error fetching companies: ");
+  }
+}
+
+export async function adminCreateCompany(
+  companyData: CompanyBase,
+): Promise<GetCompany> {
+  const formData = new FormData();
+  formData.append("name", companyData.name);
+  formData.append("AFM", companyData.AFM);
+
+  try {
+    const response = await fetch(API_URLS.COMPANY_BASE_URL, {
+      method: "POST",
+      credentials: "include",
+      body: formData,
+    });
+
+    if (!response.ok) {
+      const errorResponse: ErrorResponse = await extractErrorMessage(response);
+      return errorResponse;
+    }
+    const data: GetCompany = await response.json();
+    return data;
+  } catch (error) {
+    errorLog("Error creating company:", error);
+    return { error: "An unexpected error occurred during company create." };
+  }
+}
+export async function adminUpdateCompany(
+  companyID: number,
+  companyData: CompanyBase,
+): Promise<GetCompany> {
+  const formData = new FormData();
+  formData.append("name", companyData.name);
+  formData.append("AFM", companyData.AFM);
+
+  try {
+    const response = await fetch(`${API_URLS.COMPANY_BASE_URL}/${companyID}`, {
+      method: "PUT",
+      credentials: "include",
+      body: formData,
+    });
+
+    if (!response.ok) {
+      const errorResponse: ErrorResponse = await extractErrorMessage(response);
+      return errorResponse;
+    }
+    const data: GetCompany = await response.json();
+    return data;
+  } catch (error) {
+    errorLog("Error creating company:", error);
+    return { error: "An unexpected error occurred during company create." };
+  }
+}
+export async function adminDeleteCompany(
+  companyID: number,
+): Promise<Message | ErrorResponse> {
+  try {
+    const response = await fetch(
+      `${API_URLS.COMPANY_BASE_URL}/delete/${companyID}`,
+      {
+        method: "Get",
+        credentials: "include",
+      },
+    );
+
+    if (!response.ok) {
+      const errorResponse: ErrorResponse = await extractErrorMessage(response);
+      return errorResponse;
+    }
+    const data: Message = await response.json();
+    return data;
+  } catch (error) {
+    errorLog("Error creating company:", error);
+    return { error: "An unexpected error occurred during company create." };
   }
 }
