9a21c25394e02a1fb68bb65bc0b458c09e746890
michaeltheo <45629601+michaeltheo@users.noreply.github.com>
2024-06-07 12:05:01 +0300

    feat: add comments to opt service

--- services/otpService.ts
+++ services/otpService.ts
@@ -3,8 +3,14 @@ import { extractErrorMessage } from "@/services/errorHandling";
 import { GenerateOTPResponse, ValidateOTP } from "@/types/otp";
 import { ErrorResponse } from "@/types";
 
+/**
+ * Generates a One-Time Password (OTP).
+ *
+ * @returns {Promise<GenerateOTPResponse>} The response containing the OTP or an error message.
+ */
 export async function generateOTP(): Promise<GenerateOTPResponse> {
   try {
+    // Fetch the OTP generation endpoint
     const response = await fetch(API_URLS.OTP_GENERATE, {
       method: "GET",
       credentials: "include",
@@ -12,18 +18,26 @@ export async function generateOTP(): Promise<GenerateOTPResponse> {
         Accept: "application/json",
       },
     });
+
     if (!response.ok) {
       const errorResponse: ErrorResponse = await extractErrorMessage(response);
       return errorResponse;
     }
+
     const data: GenerateOTPResponse = await response.json();
     return data;
   } catch (error) {
-    errorLog("Error generating  OTP:", error);
+    errorLog("Error generating OTP:", error);
     return { error: "An unexpected error occurred during OTP generation." };
   }
 }
 
+/**
+ * Validates the provided OTP code.
+ *
+ * @param {string} otpCode - The OTP code to be validated.
+ * @returns {Promise<ValidateOTP>} The response indicating the validation result or an error message.
+ */
 export async function validateOTP(otpCode: string): Promise<ValidateOTP> {
   try {
     const response = await fetch(`${API_URLS.OTP_VALIDATE}/${otpCode}`, {
@@ -32,14 +46,16 @@ export async function validateOTP(otpCode: string): Promise<ValidateOTP> {
         Accept: "application/json",
       },
     });
+
     if (!response.ok) {
       const errorResponse: ErrorResponse = await extractErrorMessage(response);
       return errorResponse;
     }
+
     const data: ValidateOTP = await response.json();
     return data;
   } catch (error) {
-    errorLog("Error generating  OTP:", error);
-    return { error: "An unexpected error occurred during OTP generation." };
+    errorLog("Error validating OTP:", error);
+    return { error: "An unexpected error occurred during OTP validation." };
   }
 }
